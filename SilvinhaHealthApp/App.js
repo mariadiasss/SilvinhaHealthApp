b39186653ef831a8062d308ac6ab69324c8a3921
mariadiasss <mariacoelhodias94@gmail.com>
2025-03-14 12:26:12 -0300

    Finalizada a interface gráfica

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity, } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Ionicons from "@expo/vector-icons?Ionicons";
 
@@ -35,6 +36,11 @@ export default function App() {
         <Ionicons name="calculator-sharp" size={24} color="#edf2f4"/>
         <Text style={styles.text}>Calcular</Text>
       </TouchableOpacity>
+
+      <Viem style={styles.imcContainer}>
+        <Text style={styles.imcText}>Preencha o peso e a altura</Text>
+        <Text style={styles.imcResult}></Text>
+      </Viem>
 
       </View>
       <StatusBar style='light'/>
@@ -101,5 +107,21 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginLeft: 5,
-  }
+  },
+  imcContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  imcText:{
+    fontSize: 18,
+    color: '#ef233c',
+    fontWeight: 'bold',
+  },
+  imcResult:{
+    fontSize: 48,
+    color: '#ef233c',
+    fontWeight: 'bold',
+  },
 });
