604e308b22fd497c0e992da8b54fc7b5612d12f9
mariadiasss <mariacoelhodias94@gmail.com>
2025-03-26 13:55:39 -0300

    Projeto FInalizado

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity, Keyboard, } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity, Keyboard} from 'react-native';
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function App() {
   const [height, setHeight] = useState(null);
   const [weight, setWeight] = useState(null);
-  const [imc, setImc] = useState(null)
-  const [messageImc, setMessageImc] = useState("Preencha o peso e a altura");
+  const [imc, setImc] = useState(null);
+  const [messageImc, setMessageImc] = useState("preencha o peso e a altura");
   const [textButton, setTextButton] = useState("Calcular");
 
-  function calcularImc(){
+  function calcularImc() {
     if (weight != null && height != null) {
       Keyboard.dismiss();
       setImc((weight / (height * height)).toFixed(2));
@@ -20,55 +20,56 @@ export default function App() {
       setTextButton("Calcular Novamente");
     } else {
       setImc(null);
-      setMessageImc("Preencha o peso e altura");
+      setMessageImc("preenche o peso e altura");
       setTextButton("Calcular");
     }
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Silvinha Health App</Text>
+      <View style={styles.titlecontainer}>
+       <Text style={styles.title}>Silvinha Health App</Text>
       </View>
       <View style={styles.content}>
-        <Text style={styles.subTitle}>Calculadora de IMC</Text>
-        
-      <View>
-        <Text style={styles.label}>Altura</Text>
-        <TextInput
-          style={styles.input}
-          value={height ?? ''}
-          onChangeText={setHeight}
-          placeholder='Ex. 1.70'
-          keyboardType='numeric'
-        ></TextInput>
-      </View>
-        <View style={{marginTop: 25}}>
-        <Text style={styles.label}>Peso</Text>
-        <TextInput
-          style={styles.input}
-          value={weight ?? ''}
-          onChangeText={setWeight}
-          placeholder='Ex. 80.650'
-          keyboardType='numeric'
-        ></TextInput>
-      </View>
+       <Text style={styles.subtitle}>Calculadora de IMC</Text>
 
-      <TouchableOpacity 
-        style={styles.button}
-        onPress={()=> calcularImc()}
-      >
-        <Ionicons name="calculator-sharp" size={24} color="#edf2f4"/>
-        <Text style={styles.text}>{textButton}</Text>
-      </TouchableOpacity>
+        <View>
+          <Text style={styles.label}>Altura</Text>
+          <TextInput
+            style={styles.input}
+            value={height ?? ''}
+            onChangeText={setHeight}
+            placeholder='Ex. 1.70'
+            keyboardType='numeric'
+          ></TextInput>
+        </View>
 
-      <View style={styles.imcContainer}>
-        <Text style={styles.imcText}>{messageImc}</Text>
-        <Text style={styles.imcResult}>{imc}</Text>
-      </View>
+        <View style={{ marginTop: 25 }}>
+          <Text style={styles.label}>Peso</Text>
+          <TextInput
+            style={styles.input}
+            value={weight ?? ''}
+            onChangeText={setWeight}
+            placeholder='Ex. 56Kg'
+            keyboardType='numeric'
+          ></TextInput>
+        </View>
+
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => calcularImc()}
+        >
+          <Ionicons name="calculator-sharp" size={24} color="#edf2f4" />
+          <Text style={styles.text}>{textButton}</Text>-
+        </TouchableOpacity>
+
+        <View style={styles.imcContainer}>
+          <Text style={styles.imcText}>{messageImc}</Text>
+          <Text style={styles.imcResult}>{imc}</Text>
+        </View>
 
       </View>
-      <StatusBar style='light'/>
+      <StatusBar style='light' />
     </SafeAreaView>
   );
 }
@@ -76,33 +77,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#edf2f4',
+    backgroundColor: '#EDF2F4',
+
   },
-  titleContainer: {
-    alignItems: 'center',
+  titlecontainer: {
+    alignItems:'center',
     justifyContent: 'flex-end',
     height: 130,
     backgroundColor: '#D90429',
     borderBottomStartRadius: 25,
     borderBottomEndRadius: 25,
   },
-  title: {
-    color: '#edf2f4',
+  title:{
+    color:'#EDF2F4',
     fontSize: 28,
-    fontWeight: 'bold',
+    fontWeight:'bold',
     marginBottom: 30,
   },
   content:{
     flex: 1,
     padding: 40,
-    width: '100%',
-    backgroundColor:'#edf2f4', 
+    width:'100%',
+    backgroundColor:'EDF2F4'
+    
   },
-  subTitle:{
+  subtitle:{
     textAlign: 'center',
     fontSize: 24,
-    color: '#D90429',
-    fontWeight: 'bold',
+    color:'#D90429',
+    fontWeight:'bold',
     marginBottom: 40,
   },
   label:{
@@ -111,13 +114,14 @@ const styles = StyleSheet.create({
   },
   input:{
     height: 45,
-    width: '100%',
+    width:'100%',
     fontSize: 18,
     borderColor: '#D90429',
-    borderBottomWidth: 1
+    borderBottomWidth: 1,
   },
+
   button: {
-    width:'100%',
+    width: '100%',
     paddingVertical: 15,
     flexDirection: 'row',
     justifyContent: 'center',
@@ -125,7 +129,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#ef233c',
     borderRadius: 15,
     marginTop: 40,
-    marginBottom: 10,
+    marginbottom: 10,
   },
   text: {
     color: '#edf2f4',
@@ -139,14 +143,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width: '100%',
   },
-  imcText:{
+  imcText: {
     fontSize: 18,
-    color: '#ef233c',
+    color: '#ef223c',
     fontWeight: 'bold',
   },
-  imcResult:{
+  imcResult: {
     fontSize: 48,
     color: '#ef233c',
     fontWeight: 'bold',
-  },
+  }
 });
